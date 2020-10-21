eab14b0297ecb6de1b28385c8d5013ee33af0a98
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2020-10-21 16:19:21 +0300

    Updates

---
+++ precache-manifest.0319335bbaf19104c3f2c85ee71fb740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18054365da5be97c91ceaa50db3cc025",
+    "url": "/SmallTasks/index.html"
+  },
+  {
+    "revision": "7af965a8a0fbbff81691",
+    "url": "/SmallTasks/static/css/main.5a409d3f.chunk.css"
+  },
+  {
+    "revision": "5d5dd3604bd409a9e195",
+    "url": "/SmallTasks/static/js/2.87fe8380.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/SmallTasks/static/js/2.87fe8380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af965a8a0fbbff81691",
+    "url": "/SmallTasks/static/js/main.7b2dc6c2.chunk.js"
+  },
+  {
+    "revision": "6a24f09c40d999b9c850",
+    "url": "/SmallTasks/static/js/runtime-main.b02961f9.js"
+  }
+]);
